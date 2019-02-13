46ac860b9888798916b82dd4ef9a6a8af6f81cf4
강동윤 <kdy1@outlook.kr>
2019-02-13 11:04:51 +0900

    Update

--- input.js
+++ input.js
@@ -1,13 +1,1 @@
-import foo, { test } from 'foo';
-import * as bar from 'bar';
-
-/**
- * Foo
- */
-class Foo extends
-    Bar {
-
-    foo() {
-        import("foo").then(foo => { });
-    }
-}
+const fn = ({ a, ...otherProps }) => otherProps;
