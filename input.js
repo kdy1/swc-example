7a35433c22444e546c20ad14daf1e5c3c3e7b83d
강동윤 <kdy1@outlook.kr>
2019-02-13 12:07:33 +0900

    More

--- input.js
+++ input.js
@@ -1,1 +1,19 @@
-const fn = ({ a, ...otherProps }) => otherProps;
+export class Foo {
+    func(a, b = Date.now()) {
+        return { a };
+    }
+}
+
+function func(a, b = Date.now()) {
+    return { a };
+}
+
+_createClass(Foo, [{
+    key: 'func',
+    value: function func(a, b = Date.now()) {
+
+        return {
+            a: a
+        };
+    }
+}]);
---
+++ scripts/all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+npx swc input.js inputs/**/*.js -d out
